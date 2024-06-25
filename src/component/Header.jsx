f2e826ac4104f8c4bd2692bae799263e02ebbc7b
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2024-06-25 12:45:34 +0100

    works a bit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,63 +1,137 @@
 import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import sunny from '../assets/images/flower.png';
 import logoSunny from "../assets/images/logoSunny.png";
-import { Link} from "react-router-dom";
 import Login from "../pages/Login";
 import SignInForm from "../pages/Signin";
+import 'flowbite';
+import 'flowbite-react';
 import { fetchUser } from "../utils/fetchUser";
 
 const Header = () => {
+  const user = fetchUser();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isListOpen, setIsListOpen] = useState(false);
-  const user = fetchUser();
   const [showSignIn, setShowSignIn] = useState(false);
 
+  // Toggle the SignInForm visibility
   const toggleSignIn = () => {
     setShowSignIn(!showSignIn);
   };
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-  
+  // Toggle the main menu visibility
   const toggleList = () => {
     setIsListOpen(!isListOpen);
   }
 
+  // Toggle the user menu visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
-      <header>
-        <nav className="bg-white font-[cursive] border-gray-200 dark:bg-gray-900">
-          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
-            {/* Logo */}
-            <span>
-              <a
-                href="*"
-                className="flex items-center  rtl:space-x-reverse md:ml-[-3rem]"
-              >
-                <img src={logoSunny} className="h-14 " alt="Sunny Logo" />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap text-[#3e1943] dark:text-white">
-                  Sunny
-                </span>
-              </a>
-            </span>
-            {/* Logo design ends here */}
-                
+      <div className="relative">
+        <nav className="absolute top-0 left-0 w-full bg-transparent
+         border-gray-200 text-white z-15">
+          <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-7">
             
-            {/*  For user profile */}
-            <div
-              className="relative flex items-center md:order-3 space-x-3 
-            md:space-x-0 rtl:space-x-reverse"
-            >
+            {/* Logo on the left */}
+            <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
+              <img src={sunny} className="h-8" alt="star" />
+              <span className="font-bold text-3xl">Sunny</span>
+            </a>
+
+            {/* Mobile Menu and User Menu toggles */}
+            <div className="flex items-center space-x-3 rtl:space-x-reverse md:hidden">
+              {/* Hamburger menu toggle */}
+              <button
+                onClick={toggleList}
+                type="button"
+                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-white rounded-lg hover:bg-black focus:outline-none focus:ring-2 focus:ring-white"
+                aria-controls="navbar-hamburger"
+              >
+                <span className="sr-only">Toggle menu</span>
+                {isListOpen ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
+                    <g fill="#D0D6F9" fillRule="evenodd">
+                      <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
+                      <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
+                    </g>
+                  </svg>
+                ) : (
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
+                    <g fill="#D0D6F9" fillRule="evenodd">
+                      <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
+                    </g>
+                  </svg>
+                )}
+              </button>
+
+              {/* User menu toggle */}
               <button
                 onClick={toggleMenu}
                 type="button"
-                className="flex text-sm bg-gray-800 rounded-full
-                 md:me-0 focus:ring-4
-         focus:ring-[#3e1943] dark:focus:ring-gray-600"
-                id="user-menu-button"
+                className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-[#3e1943] dark:focus:ring-gray-600"
                 aria-expanded="false"
-                data-dropdown-toggle="user-dropdown"
-                data-dropdown-placement="bottom"
+              >
+                <span className="sr-only">Open user menu</span>
+                <img
+                  className="w-8 h-8 rounded-full"
+                  src={user.picture}
+                  alt="user photo"
+                />
+              </button>
+            </div>
+
+            {/* Main Navigation Menu */}
+            <div
+              className={`${isListOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`}
+              id="navbar-hamburger"
+            >
+              <ul className="font-medium flex flex-col md:flex-row md:space-x-8 md:items-center text-center">
+                <li>
+                  <Link
+                    to='/'
+                    className="block py-2 px-3 text-white uppercase md:text-white md:p-0"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to='/about'
+                    className="block py-2 px-3 text-white uppercase md:text-white md:p-0"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to='/blog'
+                    className="block py-2 px-3 text-white uppercase md:text-white md:p-0"
+                  >
+                    Blog
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to='/contact'
+                    className="block py-2 px-3 text-white uppercase md:text-white md:p-0"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            {/* User Profile Menu for Desktop */}
+            <div className="hidden md:flex items-center space-x-3 rtl:space-x-reverse">
+              <button
+                onClick={toggleMenu}
+                type="button"
+                className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-[#3e1943] dark:focus:ring-gray-600"
+                aria-expanded="false"
               >
                 <span className="sr-only">Open user menu</span>
                 <img
@@ -67,51 +141,45 @@ const Header = () => {
                 />
               </button>
 
-              {/* <!-- Dropdown menu --> */}
               {isMenuOpen && (
                 <div
-                  className="absolute right-0 top-0.5 z-50 mt-20 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                  className="absolute right-0 top-0 z-50 mt-20 text-base 
+                  list-none bg-white divide-y divide-gray-100 
+                  rounded-lg shadow dark:bg-gray-700
+                   dark:divide-gray-600"
                   id="user-dropdown"
                 >
                   <div className="px-4 py-3">
                     <span className="block text-sm text-gray-900 dark:text-white">
-                      {user.name}
+                      {user ? user.name : "No name"}
                     </span>
                     <span className="block text-sm text-gray-500 truncate dark:text-gray-400">
-                      {user.email}
+                      {user ? user.email : "No mail"}
                     </span>
                   </div>
                   <ul className="py-2" aria-labelledby="user-menu-button">
                     <li>
-                      <Link to="/">
-                        <a
-                          href="#"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100
-                 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
-                          Home
-                        </a>
+                      <Link
+                        to="/"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      >
+                        Home
                       </Link>
                     </li>
-                    {/* Sign In button */}
                     <li>
                       <Login>
                         <a
                           href="#"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100
-               dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
                           Sign in
                         </a>
                       </Login>
                     </li>
-
                     <li>
                       <button
                         onClick={toggleSignIn}
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100
-               dark:hover:bg-gray-600 dark:text-gray-200
-                dark:hover:text-white"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                       >
                         Sign In with Email
                       </button>
@@ -120,8 +188,7 @@ const Header = () => {
                     <li>
                       <a
                         href="#"
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100
-               dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                       >
                         Sign out
                       </a>
@@ -129,200 +196,10 @@ const Header = () => {
                   </ul>
                 </div>
               )}
-              {/* Dropdown menu for profile */}
             </div>
-            {/* For user profile */}
-
-
-            {/* Mobile Design */}
-            <div  className="relative flex items-center md:order-2 space-x-3 
-            md:space-x-0 rtl:space-x-reverse">
-            <button
-                data-collapse-toggle="navbar-user"
-                onClick={toggleList}
-                type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500
-                 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                aria-controls="navbar-user"
-                id="user-menu-button"
-                aria-expanded={isListOpen ? "true" : "false"}
-                data-dropdown-toggle="user-dropdown"
-                data-dropdown-placement="bottom"
-              >
-                <span className="sr-only">Toggle Menu</span>
-                <svg
-                  className="w-5 h-5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 17 14"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 1h15M1 7h15M1 13h15"
-                  />
-                </svg>
-              </button>
-            {/* Menu starts here */}
-            {isListOpen && (
-              <div
-              className=" absolute right-0 top-0 items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-              
-              id="user-dropdown"
-            >
-              <ul
-                className="flex flex-col font-medium p-4 md:p-0 mt-4 border
-         border-gray-100 rounded-lg uppercase bg-gray-50 md:space-x-8 rtl:space-x-reverse
-          md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-              >
-                <Link to="/">
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 px-3 text-white
-         rounded md:bg-transparent md:text-[#3e1943]  font-bold
-         md:p-0 md:dark:text-white"
-                      aria-current="page"
-                    >
-                      Home
-                    </a>
-                  </li>
-                </Link>
-
-                <Link to="/about">
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 px-3 text-gray-900 rounded
-         hover:bg-gray-100 md:hover:bg-transparent
-          md:hover:text-[#3e1943] hover:font-semibold md:p-0 dark:text-white
-             dark:hover:bg-gray-700
-            dark:hover:text-white md:dark:hover:bg-transparent
-             dark:border-gray-700DDDD"
-                    >
-                      About Me
-                    </a>
-                  </li>
-                </Link>
-
-                <Link to="/blog">
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 px-3 text-gray-900 rounded
-         hover:bg-gray-100 md:hover:bg-transparent
-         md:hover:text-[#3e1943] hover:font-semibold md:p-0 dark:text-white
-           md:dark:hover:text-emerald-900 dark:hover:bg-gray-700
-            dark:hover:text-white md:dark:hover:bg-transparent
-             dark:border-gray-700"
-                    >
-                      Blogs
-                    </a>
-                  </li>
-                </Link>
-
-                <Link to="/contact">
-                  <li>
-                    <a
-                      href="#"
-                      class="block py-2 px-3 text-gray-900 rounded
-         hover:bg-gray-100 md:hover:bg-transparent 
-         md:hover:text-[#3e1943] hover:font-semibold md:p-0 dark:text-white
-          md:dark:hover:text-blue-500 dark:hover:bg-gray-700
-           dark:hover:text-white md:dark:hover:bg-transparent
-            dark:border-gray-700"
-                    >
-                      Contact
-                    </a>
-                  </li>
-                </Link>
-              </ul>
-            </div>
-
-            )}
-            {/* For mobile menu  */}
-
-               {/* for Laptop menu */}
-             <div
-              className="  items-center  justify-between hidden w-full md:flex md:w-auto md:order-1"
-              id="user-dropdown"
-            >
-              <ul
-                className="flex flex-col font-medium p-4 md:p-0 mt-4 border
-         border-gray-100 rounded-lg uppercase bg-gray-50 md:space-x-8 rtl:space-x-reverse
-          md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
-              >
-                <Link to="/">
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 px-3 text-white
-         rounded md:bg-transparent md:text-[#3e1943]  font-bold
-         md:p-0 md:dark:text-white"
-                      aria-current="page"
-                    >
-                      Home
-                    </a>
-                  </li>
-                </Link>
-
-                <Link to="/about">
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 px-3 text-gray-900 rounded
-         hover:bg-gray-100 md:hover:bg-transparent
-          md:hover:text-[#3e1943] hover:font-semibold md:p-0 dark:text-white
-             dark:hover:bg-gray-700
-            dark:hover:text-white md:dark:hover:bg-transparent
-             dark:border-gray-700DDDD"
-                    >
-                      About Me
-                    </a>
-                  </li>
-                </Link>
-
-                <Link to="/blog">
-                  <li>
-                    <a
-                      href="#"
-                      className="block py-2 px-3 text-gray-900 rounded
-         hover:bg-gray-100 md:hover:bg-transparent
-         md:hover:text-[#3e1943] hover:font-semibold md:p-0 dark:text-white
-           md:dark:hover:text-emerald-900 dark:hover:bg-gray-700
-            dark:hover:text-white md:dark:hover:bg-transparent
-             dark:border-gray-700"
-                    >
-                      Blogs
-                    </a>
-                  </li>
-                </Link>
-
-                <Link to="/contact">
-                  <li>
-                    <a
-                      href="#"
-                      class="block py-2 px-3 text-gray-900 rounded
-         hover:bg-gray-100 md:hover:bg-transparent 
-         md:hover:text-[#3e1943] hover:font-semibold md:p-0 dark:text-white
-          md:dark:hover:text-blue-500 dark:hover:bg-gray-700
-           dark:hover:text-white md:dark:hover:bg-transparent
-            dark:border-gray-700"
-                    >
-                      Contact
-                    </a>
-                  </li>
-                </Link>
-              </ul>
-            </div>
-            {/* Laptop menu ends here */}
-         </div>
-        </div>
+          </div>
         </nav>
-      </header>
+      </div>
     </>
   );
 };
